945bd7349de195f395a3816e6d3cb6e7b65267ed
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2026-05-29 20:59:36 -0300

    feat(Endereco): criando factory e validação de entidade

--- DoeSangue.Domain/Entities/Endereco.cs
+++ DoeSangue.Domain/Entities/Endereco.cs
@@ -9,5 +9,51 @@ public class Endereco
     public string Bairro { get; set; } = string.Empty;
     public string Cidade { get; set; } = string.Empty;
     public string Estado { get; set; } = string.Empty;
-    
+
+    private Endereco(Guid id, string cep, string numero, string rua, string bairro, string cidade, string estado)
+    {
+        Id = id;
+        Cep = cep;
+        Numero = numero;
+        Rua = rua;
+        Bairro = bairro;
+        Cidade = cidade;
+        Estado = estado;
+    }
+
+    public static Endereco Criar(string cep, string numero, string rua, string bairro, string cidade, string estado)
+    {
+        var endereco = new Endereco(id: Guid.NewGuid(),
+                                    cep: cep,
+                                    numero: numero,
+                                    rua: rua,
+                                    bairro: bairro,
+                                    cidade: cidade,
+                                    estado: estado);
+
+        endereco.Validar();
+
+        return endereco;
+    }
+
+    public void Validar()
+    {
+        if (string.IsNullOrWhiteSpace(Cep))
+            throw new ArgumentException("O CEP é obrigatório.");
+
+        if (string.IsNullOrWhiteSpace(Numero))
+            throw new ArgumentException("O número é obrigatório.");
+
+        if (string.IsNullOrWhiteSpace(Rua))
+            throw new ArgumentException("A rua é obrigatória.");
+
+        if (string.IsNullOrWhiteSpace(Bairro))
+            throw new ArgumentException("O bairro é obrigatório.");
+
+        if (string.IsNullOrWhiteSpace(Cidade))
+            throw new ArgumentException("A cidade é obrigatória.");
+
+        if (string.IsNullOrWhiteSpace(Estado))
+            throw new ArgumentException("O estado é obrigatório.");
+    }
 }
