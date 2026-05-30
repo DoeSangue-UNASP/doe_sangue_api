2929f208bd241867e459b7e6873dfee8034ca7b8
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2026-05-29 21:09:19 -0300

    feat(Usuario): criando factory e validação de entidade

--- DoeSangue.Domain/Entities/Usuario.cs
+++ DoeSangue.Domain/Entities/Usuario.cs
@@ -1,4 +1,6 @@
-﻿using DoeSangue.Domain.Enums;
+﻿using System.Net.Mail;
+using System.Text.RegularExpressions;
+using DoeSangue.Domain.Enums;
 
 namespace DoeSangue.Domain.Entities;
 
@@ -11,4 +13,34 @@ public class Usuario
     public DateTime? AtualizadoEm { get; set; }
 
     public Endereco? Endereco { get; set; }
+
+    private Usuario(Guid id, string email, UsuarioRole role, DateTime criadoEm, DateTime? atualizadoEm, Endereco? endereco)
+    {
+        Id = id;
+        Email = email;
+        Role = role;
+        CriadoEm = criadoEm;
+        AtualizadoEm = atualizadoEm;
+        Endereco = endereco;
+    }
+
+    public static Usuario Criar(string email, UsuarioRole role, Endereco? endereco = null)
+    {
+        var usuario = new Usuario(id: Guid.NewGuid(),
+                                  email: email,
+                                  role: role,
+                                  criadoEm: DateTime.UtcNow,
+                                  atualizadoEm: null,
+                                  endereco: endereco);
+
+        usuario.Validar();
+
+        return usuario;
+    }
+
+    public void Validar()
+    {
+        if (Regex.IsMatch(Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$") == false)
+            throw new ArgumentException("O email do usuário é inválido.");
+    }
 }
